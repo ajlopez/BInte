581fd6c8e07e3bb836c74f342e76c6014a824e46
ajlopez <webmaster@ajlopez.com>
2017-10-02 08:53:43 -0300

    Add integer value to integer object

--- test/integer.js
+++ test/integer.js
@@ -9,3 +9,14 @@ exports['create from small integer'] = function (test) {
 	test.equal(integer.toString(), '42');
 	test.equal(integer.negative(), false);
 };
+
+exports['add integer number to integer object'] = function (test) {
+	var integer = binte.integer(41);
+	
+	var result = integer.add(1);
+	
+	test.ok(result);
+	test.equal(typeof result, 'object');
+	test.equal(result.toString(), '42');
+	test.equal(result.negative(), false);
+};
