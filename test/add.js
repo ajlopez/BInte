1e79c3094ab8a1674dac9ae3393ecd49957d1fba
ajlopez <webmaster@ajlopez.com>
2017-12-08 15:07:44 -0300

    Add BigInteger to BigInteger

--- test/add.js
+++ test/add.js
@@ -11,3 +11,16 @@ exports['add integer number to integer object'] = function (test) {
 	test.equal(result.toString(), '42');
 	test.equal(result.negative(), false);
 };
+
+exports['add integer object to integer object'] = function (test) {
+	var integer = binte.integer(41);
+	var one = binte.integer(1);
+	
+	var result = integer.add(one);
+	
+	test.ok(result);
+	test.equal(typeof result, 'object');
+	test.equal(result.toString(), '42');
+	test.equal(result.negative(), false);
+};
+
