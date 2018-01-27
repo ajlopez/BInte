e5f959235d39a2656df26d436580dc4a1232825b
ajlopez <webmaster@ajlopez.com>
2018-01-27 10:59:02 -0300

    Subtract big integer from big integer

--- test/subtract.js
+++ test/subtract.js
@@ -12,3 +12,13 @@ exports['add subtract integer number to integer object'] = function (test) {
 	test.equal(result.negative(), false);
 };
 
+exports['add subtract big integer number from big integer object'] = function (test) {
+	var integer = binte.integer(2000000000000043);
+	
+	var result = integer.subtract(binte.integer(2000000000000001));
+	
+	test.ok(result);
+	test.equal(typeof result, 'object');
+	test.equal(result.toString(), '42');
+	test.equal(result.negative(), false);
+};
