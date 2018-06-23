4aeb3e6eabf0958df6f7daf55019f2ea3e4df85b
ajlopez <webmaster@ajlopez.com>
2018-06-23 13:14:40 -0300

    Multiply by negative number

--- test/multiply.js
+++ test/multiply.js
@@ -11,3 +11,14 @@ exports['multiply integer number by integer object'] = function (test) {
 	test.equal(result.toString(), '42');
 	test.equal(result.negative(), false);
 };
+
+exports['multiply integer number by negative integer object'] = function (test) {
+	var integer = binte.integer(21);
+	
+	var result = integer.multiply(-2);
+	
+	test.ok(result);
+	test.equal(typeof result, 'object');
+	test.equal(result.toString(), '-42');
+	test.equal(result.negative(), true);
+};
