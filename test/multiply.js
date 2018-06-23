a56bf8f5b1ff84beb633d1f81c89efa13352ca2f
ajlopez <webmaster@ajlopez.com>
2018-06-23 13:10:52 -0300

    Multiply by number

---
+++ test/multiply.js
@@ -0,0 +1,13 @@
+
+var binte = require('..');
+
+exports['multiply integer number by integer object'] = function (test) {
+	var integer = binte.integer(21);
+	
+	var result = integer.multiply(2);
+	
+	test.ok(result);
+	test.equal(typeof result, 'object');
+	test.equal(result.toString(), '42');
+	test.equal(result.negative(), false);
+};
