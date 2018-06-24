8d57ab568de59a1e58541cc9a4fbce4213aa6a95
ajlopez <webmaster@ajlopez.com>
2018-06-24 15:21:25 -0300

    Multiply by 10, WIP

--- test/multiply.js
+++ test/multiply.js
@@ -22,3 +22,15 @@ exports['multiply integer number by negative integer object'] = function (test) 
 	test.equal(result.toString(), '-42');
 	test.equal(result.negative(), true);
 };
+
+exports['multiply integer number by ten many times'] = function (test) {
+	var integer = binte.integer(1);
+	var expected = '1';
+	
+	for (var k = 0; k < 32; k++) {
+		integer = integer.multiply(10);
+		expected = expected + '0';
+		test.equal(integer.toString(), expected);
+		test.equal(integer.negative(), false);
+	}
+};
